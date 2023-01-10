dbc1d56cbf3811328d58fdba0383b2797c4ed11a
kipngeno kevin <kipngenokevin254@gmail.com>
2023-01-10 11:53:58 +0300

    Portfolio section updated

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,10 +1,83 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.jpg'
+import IMG6 from '../../assets/portfolio6.jpg'
 
-function portfolio() {
+
+
+const Portfolio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt='portfolio'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
+            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
+          </div>         
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt='portfolio'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
+            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
+          </div>         
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt='portfolio'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
+            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
+          </div>         
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt='portfolio'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
+            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
+          </div>         
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt='portfolio'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
+            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
+          </div>         
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt='portfolio'/>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
+            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
+          </div>         
+        </article>
+      </div>
+    </section>
   )
 }
 
-export default portfolio
+export default Portfolio
