7d6bc456a0ebf488cc60cdbab483424b5bf976db
kipngeno kevin <kipngenokevin254@gmail.com>
2023-01-11 16:16:37 +0300

    Testimonial section completed

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -7,6 +7,51 @@ import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.jpg'
 import IMG6 from '../../assets/portfolio6.jpg'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: '',
+    github: 'https://github.com/kipngenokevin',
+    demo: ''
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Card ID Services Mobile App Concept',
+    github: '',
+    demo: ''
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Card ID Services Mobile App Concept',
+    github: '',
+    demo: ''
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Card ID Services Mobile App Concept',
+    github: '',
+    demo: ''
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Card ID Services Mobile App Concept',
+    github: '',
+    demo: ''
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Card ID Services Mobile App Concept',
+    github: '',
+    demo: ''
+  }
+]
+
 
 
 const Portfolio = () => {
@@ -15,66 +60,26 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
-            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
-          </div>         
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
-            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
-          </div>         
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
-            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
-          </div>         
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
-            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
-          </div>         
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
-            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
-          </div>         
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com/kipngenokevin' className='btn' target='_blank'>Github</a>
-            <a href='https://www.instagram.com/kipngeno.kevin/' className='btn btn-primary' target='_blank'>Instagram</a>
-          </div>         
-        </article>
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio__item-image'>
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Instagram</a>
+                </div>
+              </article>
+            )
+          })
+        }
+
+
+
+
       </div>
     </section>
   )
--- src/components/testimonials/testimonial.jsx
+++ src/components/testimonials/testimonial.jsx
@@ -1,10 +1,73 @@
 import React from 'react'
 import './testimonial.css'
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
 
-const testimonial = () => {
-  return (
-    <section id='testimonial'>testimonial</section>
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+const data = [
+  {
+    id: '1',
+    image: AVTR1,
+    client_name: 'John Alan',
+    client_review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    id: '2',
+    image: AVTR2,
+    client_name: 'John Alan',
+    client_review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    id: '3',
+    image: AVTR3,
+    client_name: 'John Alan',
+    client_review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    id: '4',
+    image: AVTR4,
+    client_name: 'John Alan',
+    client_review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }
+]
+
+const Testimonial = () => {
+  return(
+    <section id ='testimonial'>
+      *<h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper 
+          className='container testimonials__container'
+          modules={[Pagination]}
+          spaceBetween={40}
+          slidesPerView={1}
+          pagination={{ clickable: true }}>
+         {
+          data.map(({id, image, client_name, client_review}) => {
+            return(
+              <SwiperSlide key={id} className='testimonial'>
+                <div className='client__avatar'>
+                  <img src={image} alt ='Avatar one'/>
+               </div>
+                <h5 className ='client__name'>{client_name}</h5>
+                <small className='client__review'>{client_review} </small>
+             </SwiperSlide>
+            )
+          })
+         }
+      </Swiper> 
+    </section>
   )
 }
 
-export default testimonial
+export default Testimonial
