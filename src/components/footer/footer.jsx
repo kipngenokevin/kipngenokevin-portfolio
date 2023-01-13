20a858d4f67e663ba43df88ee05913768725046b
kipngeno kevin <kipngenokevin254@gmail.com>
2023-01-13 16:45:46 +0300

    Project Done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebook} from 'react-icons/fa'
+import {BsInstagram, BsTwitter, BsLinkedin} from 'react-icons/bs'
 
-function footer() {
+const Footer = () => {
   return (
-    <section id='footer'>footer</section>
+   <footer>
+    <a href='#' className='footer__logo'> Kevin Kipngeno</a>
+    <ul className='permalinks'>
+      <li><a href='#'>Home</a></li>
+      <li><a href='#about'>About</a></li>
+      <li><a href='#experience'>Experience</a></li>
+      <li><a href='#services'>Services</a></li>
+      <li><a href='#portfolio'>Portfolio</a></li>
+      <li><a href='#testimonials'>Testimonials</a></li>
+      <li><a href='#contact'>Contact</a></li>
+    </ul>
+    <div className='footer__socials'>
+      <a href='https://www.instagram.com/kipngeno.kevin/'><BsInstagram/></a>
+      <a href='https://www.facebook.com/kipngeno.kevin/'><FaFacebook/></a>
+      <a href='https://www.twitter.com/kipngeno.kevin/'><BsTwitter/></a>
+      <a href='https://www.linkedin.com/kipngeno.kevin/'><BsLinkedin/></a>
+    </div>
+    <div className='footer__copyright'>
+      <small>&copy; Kipngeno Kevin all rights reserved.</small>
+    </div>
+   </footer>
   )
 }
 
-export default footer
+export default Footer
